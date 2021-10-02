7625034c522e19773eabdccaa48361b8c0604163
Jovica Jovićević <jovica.jovicevic@googlemail.com>
2021-10-02 17:56:51 +0200

    Add database migration

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 mail = Mail(app)
 
 from app import app
