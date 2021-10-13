8034d4b897883609444e5b9eff608ba01a0e20e6
Jovica Jovićević <jovica.jovicevic@googlemail.com>
2021-10-13 15:52:32 +0200

    Add login test

--- tests/tests.py
+++ tests/tests.py
@@ -24,3 +24,29 @@ class TestNoLogin(unittest.TestCase):
 
 	if __name__ == '__main__':
 		unittest.main()
+
+
+class TestLogin(unittest.TestCase):
+	def setUp(self):
+		os.environ['NO_PROXY'] = '127.0.0.1'
+
+	def test_login_good(self):
+		req = requests.post("http://127.0.0.1:5000/api/arrangements/login",
+			json = {"username": "marko", "password": "marko123"})
+		self.assertEqual(req.status_code, 200)
+
+	def test_login_bad_password(self):
+		req = requests.post("http://127.0.0.1:5000/api/arrangements/login",
+			json = {"username": "marko", "password": "marko1234"})
+		self.assertEqual(req.status_code, 406)
+
+	def test_login_bad_username(self):
+		req = requests.post("http://127.0.0.1:5000/api/arrangements/login",
+			json = {"username": "mark", "password": "marko123"})
+		self.assertEqual(req.status_code, 404)
+
+	def tearDown(self):
+		requests.get("http://127.0.0.1:5000/api/arrangements/logout")
+
+	if __name__ == '__main__':
+		unittest.main()
